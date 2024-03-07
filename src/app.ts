f3cdc546da9ce846fdb98b13fac6bd50e7f768ce
Thejas Kiran P S <thejaskiranps@gmail.com>
2024-03-07 14:38:35 +0530

    disable cors

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,25 @@
 import F from "fastify"
 import { registerRoutes } from "./registerRoutes";
-
+import cors from "@fastify/cors"
 const server = F()
 
+server.register(cors, {
+  origin: '*'
+})
 
 registerRoutes(server);
 server.get('/ping', async function() {
-    return { status: "OK" };
+  return { status: "OK" };
 })
 
 async function main() {
-
-    try {
-        await server.listen({ port: 3001,host: '0.0.0.0' });
-        console.log(`Server ready`)
-    } catch (e) {
-        console.error(e)
-        process.exit(1)
-    }
+  try {
+    await server.listen({ port: 3001, host: '0.0.0.0' });
+    console.log(`Server ready`)
+  } catch (e) {
+    console.error(e)
+    process.exit(1)
+  }
 }
 
 main()
